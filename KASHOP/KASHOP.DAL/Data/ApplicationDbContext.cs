3072bc2ace4bb5ae2e72b4ab7cd1c2c5a200b542
Your Name <you@example.com>
2025-12-25 20:21:32 +0200

    Audit

--- KASHOP/KASHOP.DAL/Data/ApplicationDbContext.cs
+++ KASHOP/KASHOP.DAL/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using KASHOP.DAL.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +14,14 @@ namespace KASHOP.DAL.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,IHttpContextAccessor httpContextAccessor)
             : base(options)
         {
+            _httpContextAccessor=httpContextAccessor;
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -31,6 +35,27 @@ namespace KASHOP.DAL.Data
             builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
             builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins"); 
             builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        }
+
+
+        public override int SaveChanges()
+        {
+            var auditEntries = ChangeTracker.Entries<BaseModel>();
+            var currentUserId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            foreach (var auditEntry in auditEntries)
+            {
+                if (auditEntry.State == EntityState.Added) {
+                    auditEntry.Property(x => x.CreatedBy).CurrentValue= currentUserId;
+                    auditEntry.Property(x => x.CreatedAt).CurrentValue= DateTime.UtcNow;
+                }
+                if (auditEntry.State == EntityState.Modified) {
+                    auditEntry.Property(x => x.UpdatedBy).CurrentValue= currentUserId;
+                    auditEntry.Property(x => x.UpdatedAt).CurrentValue= DateTime.UtcNow;
+                }
+
+            }
+
+            return base.SaveChanges();
         }
     }
 }
